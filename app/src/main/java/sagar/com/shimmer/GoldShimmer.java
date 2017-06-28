4a73d8904bab9b42f13dcf23f45ea7b31f930651
Sagar Hatekar <sagar.hatekar@gotinder.com>
2017-06-28 12:41:24 -0700

    first commit

---
+++ app/src/main/java/sagar/com/shimmer/GoldShimmer.java
@@ -0,0 +1,12 @@
+package sagar.com.shimmer;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.widget.FrameLayout;
+
+public class GoldShimmer extends FrameLayout {
+
+  public GoldShimmer(@NonNull Context context) {
+    super(context);
+  }
+}
