6a0ddd0fcb1bc50565593c9753d03f6e3e26a055
sagar <sagar.hatekar@gmail.com>
2017-06-29 17:40:56 -0700

    toggle start/stop animation

--- app/src/main/java/sagar/com/shimmer/MainActivity.java
+++ app/src/main/java/sagar/com/shimmer/MainActivity.java
@@ -2,15 +2,33 @@ package sagar.com.shimmer;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+  ShimmerFrameLayout container1;
+  boolean stopped = false;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    ShimmerFrameLayout container1 =
+    container1 =
         (ShimmerFrameLayout) findViewById(R.id.shimmer_view_container1);
     container1.startShimmerAnimation();
+
+    ImageView imageView = (ImageView) findViewById(R.id.image1);
+    imageView.setOnClickListener(this);
+  }
+
+  public void onClick(View view) {
+    if (stopped) {
+      container1.startShimmerAnimation();
+      stopped = false;
+    } else {
+      container1.stopShimmerAnimation();
+      stopped = true;
+    }
   }
 }
